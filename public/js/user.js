baaeace7e62f2b8ea16f46360669d7cae65e2373
Andy <andyyuan2017@gmail.com>
2020-03-02 20:31:55 +1100

    passport done with help of tonnette

--- public/js/user.js
+++ public/js/user.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    var verifyUserArr;
     $("#signUpBtn").on("click", function(event) {
         event.preventDefault();
         var newUser = {
@@ -7,44 +8,58 @@ $(document).ready(function() {
             password: $("#pass").val().trim()
         };
 
-        $.post("/api/user", newUser)
+        if (!newUser.email || !newUser.password) {
+            return;
+        }
+        signUpUser(newUser);
+
+    });
+
+    function signUpUser(newUser) {
+        $.post("/api/signup", newUser)
             .then(function() {
                 $(".userPlace").hide();
                 $(".userName").append("<i class=\"fas fa-user\"></i> " + newUser.name);
 
-                $.get("/api/user/" + newUser.name, function(data) {
+                $.get("/api/signup/" + newUser.email, function(data) {
                     $(".userName").attr("value", data.id);
                 });
-            }).catch(function(err) {
-                console.log(err);
-            });
-    });
+            }).catch(handleLoginErr);
+    };
+
+    function handleLoginErr(err) {
+        $("#alert .msg").text(err.responseJSON);
+        $("#alert").fadeIn(500);
+    };
 
     $("#loginBtn").on("click", function(event) {
         event.preventDefault();
-        var verifyUser = {
-            name: $("#loginName").val().trim(),
+        verifyUser = {
+            email: $("#loginEmail").val().trim(),
             password: $("#loginPass").val().trim()
         };
+        verifyUserArr = verifyUser.email;
+        if (!verifyUser.email || !verifyUser.password) {
+            return;
+        };
+        loginUser(verifyUser)
 
-        $.get("/api/users", function(data) {
-            for (var i = 0; i < data.length; i++) {
-                if (data[i].name == verifyUser.name && data[i].password == verifyUser.password) {
-                    $(".userPlace").hide();
-                    $(".userName").append("<i class=\"fas fa-user\"></i> " + verifyUser.name);
-                    $(".userName").attr("value", data[i].id);
-                }
-            }
-            var temp = $(".userName").text();
-
-            if (temp == "") {
-                M.toast({ html: 'Login failed! Please login again' });
-            }
-        }).catch(function(err) {
-            console.log(err);
-        });
     });
 
+    function loginUser(verifyUser) {
+        $.post("/api/login", verifyUser)
+            .then(function(verifyUser) {
+                window.location.replace("/members");
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    }
+
+
+    // /member/:id
+    // - handlebars render with id
+    // - html + [js]()
 
 
     //document.ready root, don't go over this
