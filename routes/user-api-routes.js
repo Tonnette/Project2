d8b1b0c4476be607bf98f6a9606dd868e092d498
Andy <andyyuan2017@gmail.com>
2020-03-02 20:53:21 +1100

    backend finished

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,12 +1,24 @@
 var db = require("../models");
+var passport = require("../config/passport");
 
 module.exports = function(app) {
     //add new user
-    app.post("/api/user", function(req, res) {
+    app.post("/api/signup", function(req, res) {
         db.Users.create(req.body).then(function(addUser) {
-            res.json(addUser);
-        });
+                res.json(addUser);
+            }).then(function() {
+                res.redirect(307, "/api/login");
+            })
+            .catch(function(err) {
+                res.status(401).json(err);
+            });
     });
+
+    app.post("/api/login", passport.authenticate("local"), function(req, res) {
+        res.json(req.user);
+        console.log(req.user);
+    });
+
 
     //get all users
     app.get("/api/users", function(req, res) {
@@ -15,16 +27,36 @@ module.exports = function(app) {
         });
     });
 
+    app.get("/logout", function(req, res) {
+        req.logout();
+        res.redirect("/");
+    });
+
     //get single user
-    app.get("/api/user/:name", function(req, res) {
+    app.get("/api/signup/:email", function(req, res) {
         db.Users.findOne({
             where: {
-                name: req.params.name
+                email: req.params.email
             }
         }).then(function(dbAuthor) {
             console.log(dbAuthor)
             res.json(dbAuthor);
         });
+    });
+
+    app.get("/api/user_data", function(req, res) {
+        if (!req.user) {
+            // The user is not logged in, send back an empty object
+            res.json({});
+        } else {
+            // Otherwise send back the user's email and id
+            // Sending back a password, even a hashed password, isn't a good idea
+            res.json({
+                name: req.user.name,
+                email: req.user.email,
+                id: req.user.id
+            });
+        }
     });
 
 };
